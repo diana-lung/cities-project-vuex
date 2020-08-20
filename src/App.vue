837012a742b8e542245db9ffdb21ae24637aed5c
Diana <lung.diana.anamaria@gmail.com>
2020-08-20 14:46:00 +0300

    Operations list

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Cities <span class="diff-color">List</span></h1>
+    <list :list="getCities"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import List from './components/List.vue';
+import { mapGetters } from 'vuex';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    'list': List,
+  },
+  computed: {
+    ...mapGetters([
+      'getCities'
+    ]),
+  },
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Raleway&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Raleway', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  text-align: center;
+}
+h1 {
+  color: #2A363B;
+  font-family: 'Montserrat', sans-serif;
+  text-align: center;
   margin-top: 60px;
-  background-attachment: fixed;
+  margin-bottom: 50px;
+}
+.diff-color {
+  color: #E84A5F;
 }
 </style>
